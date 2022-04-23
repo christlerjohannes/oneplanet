4732d2abad5e6df48357f84ff23a6c995bc657f7
Johannes Christler <christler.johannes@gmail.com>
2022-04-23 17:51:10 +0200

    TEst

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,20 @@
 <template>
-  <Tutorial/>
+    <h1>ONE PLANET</h1>
 </template>
 
 <script>
 export default {
-  name: 'IndexPage'
+    name: "oneplanet",
 }
 </script>
+
+<style lang="scss" scoped>
+@font-face {
+    font-family: Joschua_Gumpert_Schriftart;
+    src: url("static/font.otf") format("opentype");
+}
+
+h1 {
+    font-family: Joschua_Gumpert_Schriftart;
+}
+</style>
